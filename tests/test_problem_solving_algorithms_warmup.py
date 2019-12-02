40f8a736f4ef4d16ed32c64760788eef6815973b
Michał Dziadowicz <michal@dziadowicz.it>
2019-12-02 21:49:10 +0100

    Add test for Warmup - Q1

---
+++ tests/test_problem_solving_algorithms_warmup.py
@@ -0,0 +1,6 @@
+from problem_solving.algorithms.warmup import *
+
+
+def test_solve_me_first():
+    assert 5 == q1_solve_me_first.solveMeFirst(2, 3)
+    assert 1100 == q1_solve_me_first.solveMeFirst(100, 1000)
