5cc0a5c60bbf0e496956367171c5a1050e238704
Michał Dziadowicz <michal@dziadowicz.it>
2019-12-03 19:46:22 +0100

    Add Warmup - Q2

--- problem_solving/algorithms/warmup/__init__.py
+++ problem_solving/algorithms/warmup/__init__.py
@@ -1,3 +1,4 @@
 __all__ = [
     'q1_solve_me_first',
+    'q2_simple_array_sum'
 ]
--- tests/test_problem_solving_algorithms_warmup.py
+++ tests/test_problem_solving_algorithms_warmup.py
@@ -4,3 +4,7 @@ from problem_solving.algorithms.warmup import *
 def test_solve_me_first():
     assert 5 == q1_solve_me_first.solveMeFirst(2, 3)
     assert 1100 == q1_solve_me_first.solveMeFirst(100, 1000)
+
+
+def test_simple_array_sum():
+    assert 31 == q2_simple_array_sum.simpleArraySum([1, 2, 3, 4, 10, 11])
