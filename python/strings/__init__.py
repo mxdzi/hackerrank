bfd265844cce5891372b05c2fa775cbebad50f10
Michał Dziadowicz <michal@dziadowicz.it>
2019-12-06 21:32:41 +0100

    Add Python Strings - Q3

--- python/strings/__init__.py
+++ python/strings/__init__.py
@@ -1,4 +1,5 @@
 __all__ = [
     'q1_swap_case',
-    'q2_python_string_split_and_join'
+    'q2_python_string_split_and_join',
+    'q3_whats_your_name'
 ]
--- tests/test_python_strings.py
+++ tests/test_python_strings.py
@@ -7,3 +7,10 @@ def test_q1_swap_case():
 
 def test_q2_python_string_split_and_join():
     assert "this-is-a-string" == q2_python_string_split_and_join.split_and_join("this is a string")
+
+
+def test_q3_whats_your_name(capsys):
+    q3_whats_your_name.print_full_name("Ross", "Taylor")
+    captured = capsys.readouterr()
+    output = "Hello Ross Taylor! You just delved into python.\n"
+    assert captured.out == output
