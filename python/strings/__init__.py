2b7e9044bfec2b3b2f7c892b754e253a2c830a2e
Michał Dziadowicz <michal@dziadowicz.it>
2019-12-06 21:25:28 +0100

    Add Python Strings - Q2

--- python/strings/__init__.py
+++ python/strings/__init__.py
@@ -1,3 +1,4 @@
 __all__ = [
-    'q1_swap_case'
+    'q1_swap_case',
+    'q2_python_string_split_and_join'
 ]
--- tests/test_python_strings.py
+++ tests/test_python_strings.py
@@ -3,3 +3,7 @@ from python.strings import *
 
 def test_q1_swap_case():
     assert 'hACKERrANK.COM PRESENTS "pYTHONIST 2".' == q1_swap_case.swap_case('HackerRank.com presents "Pythonist 2".')
+
+
+def test_q2_python_string_split_and_join():
+    assert "this-is-a-string" == q2_python_string_split_and_join.split_and_join("this is a string")
