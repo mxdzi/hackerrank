06f09f42c37ffaf6aed2efaaf06c149360cca881
Michał Dziadowicz <michal@dziadowicz.it>
2020-01-05 22:22:52 +0100

    Add SQL Advanced Select - Q1

---
+++ sql/advanced_select/q1_what_type_of_triangle.sql
@@ -0,0 +1,9 @@
+SELECT
+    CASE
+        WHEN a + b <= c OR a + c <= b OR b + c <= a THEN 'Not A Triangle'
+        WHEN a = b AND b = c THEN 'Equilateral'
+        WHEN a = b OR a = c OR b = c THEN 'Isosceles'
+        ELSE 'Scalene'
+    END
+FROM
+    triangles;
