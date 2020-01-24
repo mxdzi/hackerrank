007a97169ef475d95f01d563f459b26a6729872e
Michał Dziadowicz <michal@dziadowicz.it>
2020-01-24 22:57:56 +0100

    Add Data Structures Trees - Q4

--- tests/test_problem_solving_data_structures_trees.py
+++ tests/test_problem_solving_data_structures_trees.py
@@ -45,3 +45,29 @@ def test_q3_tree_inorder_traversal(capsys, monkeypatch):
     captured = capsys.readouterr()
     output = "1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 "
     assert captured.out == output
+
+
+def test_q4_tree_height_of_a_binary_tree(capsys, monkeypatch):
+    inputs = ["7", "3 5 2 1 4 6 7"]
+    monkeypatch.setattr('builtins.input', lambda: inputs.pop(0))
+
+    q4_tree_height_of_a_binary_tree.main()
+    captured = capsys.readouterr()
+    output = "3\n"
+    assert captured.out == output
+
+    inputs = ["1", "15"]
+    monkeypatch.setattr('builtins.input', lambda: inputs.pop(0))
+
+    q4_tree_height_of_a_binary_tree.main()
+    captured = capsys.readouterr()
+    output = "0\n"
+    assert captured.out == output
+
+    inputs = ["5", "3 1 7 5 4"]
+    monkeypatch.setattr('builtins.input', lambda: inputs.pop(0))
+
+    q4_tree_height_of_a_binary_tree.main()
+    captured = capsys.readouterr()
+    output = "3\n"
+    assert captured.out == output
