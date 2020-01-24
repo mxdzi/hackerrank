fa1d1873ba0d4b5d8b67bf9ddbc9f86d527aa4b6
Michał Dziadowicz <michal@dziadowicz.it>
2020-01-24 22:32:21 +0100

    Add Data Structures Trees - Q3

--- problem_solving/data_structures/trees/__init__.py
+++ problem_solving/data_structures/trees/__init__.py
@@ -1,4 +1,5 @@
 __all__ = [
     'q1_tree_preorder_traversal',
-    'q2_tree_postorder_traversal'
+    'q2_tree_postorder_traversal',
+    'q3_tree_inorder_traversal'
 ]
--- tests/test_problem_solving_data_structures_trees.py
+++ tests/test_problem_solving_data_structures_trees.py
@@ -27,3 +27,21 @@ def test_q2_tree_postorder_traversal(capsys, monkeypatch):
     captured = capsys.readouterr()
     output = "2 6 5 4 9 8 12 11 10 13 7 3 15 14 1 "
     assert captured.out == output
+
+
+def test_q3_tree_inorder_traversal(capsys, monkeypatch):
+    inputs = ["6", "1 2 5 3 6 4"]
+    monkeypatch.setattr('builtins.input', lambda: inputs.pop(0))
+
+    q3_tree_inorder_traversal.main()
+    captured = capsys.readouterr()
+    output = "1 2 3 4 5 6 "
+    assert captured.out == output
+
+    inputs = ["15", "1 14 3 7 4 5 15 6 13 10 11 2 12 8 9"]
+    monkeypatch.setattr('builtins.input', lambda: inputs.pop(0))
+
+    q3_tree_inorder_traversal.main()
+    captured = capsys.readouterr()
+    output = "1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 "
+    assert captured.out == output
