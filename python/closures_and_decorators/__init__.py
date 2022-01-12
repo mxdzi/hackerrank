01fd0557a0e1c3cebd2507fd760a63747e39f75a
Michał Dziadowicz <michal@dziadowicz.it>
2022-01-12 22:06:14 +0100

    Add Python Closures and Decorators - Q2

--- python/closures_and_decorators/__init__.py
+++ python/closures_and_decorators/__init__.py
@@ -1,3 +1,4 @@
 __all__ = [
     'q1_standardize_mobile_number_using_decorators',
+    'q2_decorators_2_name_directory',
 ]
--- tests/test_python_closures_and_decorators.py
+++ tests/test_python_closures_and_decorators.py
@@ -27,3 +27,35 @@ def test_q1_standardize_mobile_number_using_decorators(capsys, monkeypatch):
               "+91 91919 19191\n"
               "+91 95936 21456\n")
     assert captured.out == output
+
+
+def test_q2_decorators_2_name_directory(capsys, monkeypatch):
+    inputs = ["3",
+              "Mike Thomson 20 M",
+              "Robert Bustle 32 M",
+              "Andria Bustle 30 F"]
+    monkeypatch.setattr('builtins.input', lambda: inputs.pop(0))
+
+    q2_decorators_2_name_directory.main()
+    captured = capsys.readouterr()
+    output = ("Mr. Mike Thomson\n"
+              "Ms. Andria Bustle\n"
+              "Mr. Robert Bustle\n")
+    assert captured.out == output
+
+    inputs = ["5",
+              "Laura Moser 50 F",
+              "Ted Moser 50 M",
+              "Yena Dixit 50 F",
+              "Diya Mirza 50 F",
+              "Rex Dsouza 51 M"]
+    monkeypatch.setattr('builtins.input', lambda: inputs.pop(0))
+
+    q2_decorators_2_name_directory.main()
+    captured = capsys.readouterr()
+    output = ("Ms. Laura Moser\n"
+              "Mr. Ted Moser\n"
+              "Ms. Yena Dixit\n"
+              "Ms. Diya Mirza\n"
+              "Mr. Rex Dsouza\n")
+    assert captured.out == output
