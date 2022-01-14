ba1256f77ac811f247a29fb347c3942d6b97d077
Michał Dziadowicz <michal@dziadowicz.it>
2022-01-14 19:21:48 +0100

    Add Python XML - Q2

--- python/xml/__init__.py
+++ python/xml/__init__.py
@@ -1,3 +1,4 @@
 __all__ = [
     'q1_xml_1_find_the_score',
+    'q2_xml2_find_the_maximum_depth',
 ]
--- tests/test_python_xml.py
+++ tests/test_python_xml.py
@@ -36,3 +36,38 @@ def test_q1_xml_1_find_the_score(capsys, monkeypatch):
     captured = capsys.readouterr()
     output = "8\n"
     assert captured.out == output
+
+
+def test_q2_xml2_find_the_maximum_depth(capsys, monkeypatch):
+    inputs = ["6",
+              "<feed xml:lang='en'>",
+              "  <title>HackerRank</title>",
+              "  <subtitle lang='en'>Programming challenges</subtitle>",
+              "  <link rel='alternate' type='text/html' href='http://hackerrank.com/'/>",
+              "  <updated>2013-12-25T12:00:00</updated>",
+              "</feed>"]
+    monkeypatch.setattr('builtins.input', lambda: inputs.pop(0))
+
+    q2_xml2_find_the_maximum_depth.main()
+    captured = capsys.readouterr()
+    output = "1\n"
+    assert captured.out == output
+
+    inputs = ["11",
+              "<feed xml:lang='en'>",
+              "  <title>HackerRank</title>",
+              "  <subtitle lang='en'>Programming challenges</subtitle>",
+              "  <link rel='alternate' type='text/html' href='http://hackerrank.com/'/>",
+              "  <updated>2013-12-25T12:00:00</updated>",
+              "  <entry>",
+              "  	<author gender='male'>Harsh</author>",
+              "    <question type='hard'>XML 1</question>",
+              "    <description type='text'>This is related to XML parsing</description>",
+              "  </entry>",
+              "</feed>"]
+    monkeypatch.setattr('builtins.input', lambda: inputs.pop(0))
+
+    q2_xml2_find_the_maximum_depth.main()
+    captured = capsys.readouterr()
+    output = "2\n"
+    assert captured.out == output
